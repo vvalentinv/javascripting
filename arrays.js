7516aa03818df8dede6d4afb338a2450afd61ec1
Valentin Vlad <valentin_gabriel.vlad@yahoo.com>
2021-10-13 16:28:49 +0000

    Initial commit with all my work so far

---
+++ arrays.js
@@ -0,0 +1,26 @@
+// # JAVASCRIPTING
+
+// ## ARRAYS (Exercise 11 of 20)
+
+//  An array is a list of values. Here's an example:
+
+//     const pets = ['cat', 'dog', 'rat']
+
+// ### The challenge:
+
+//  Create a file named arrays.js.
+
+//  In that file define a variable named pizzaToppings that references an
+//  array that contains three strings in this order: tomato sauce, cheese,
+//  pepperoni.
+
+//  Use console.log() to print the pizzaToppings array to the terminal.
+
+//  Check to see if your program is correct by running this command:
+
+//     javascripting verify arrays.js
+
+// //
+
+let pizzaToppings = ["tomato sauce", "cheese", "pepperoni"];
+console.log(pizzaToppings);
---
+++ if-statement.js
@@ -0,0 +1,4 @@
+let fruit = "orange";
+let rez6orMore = "The fruit name has more than five characters.";
+let rez5orLess = "The fruit name has five characters or less.";
+fruit.length > 5 ? console.log(rez6orMore) : console.log(rez5orLess);
